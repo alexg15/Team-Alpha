b1bae6d6453a7f9751fc47e0b925f5b9bc7d8f47
dyermaker504 <dyermaker504@gmail.com>
2023-03-26 23:57:55 -0500

    Add files via upload

---
+++ plp/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>UNT Professional Leadership program </title>
+		<meta name="description" content="Professional Leadership program">
+		<meta name="keywords" content="Professional Leadership Program UNT">
+		
+		  <link rel="stylesheet" type="text/css" href="./Welcome_files/Main.css">
+      <link href="./Welcome_files/css" rel="stylesheet" type="text/css">
+		<style></style>
+	</head>
+	<body>
+		<div class="header">
+
+			<ul class="tabs">
+				<li> <b>Welcome </b></li>
+			  <li><a class="button" href="https://untplp.com/requirements.html">Requirements  </a> </li>
+			  <li><a class="button" href="BasicInfo.php">Basic Info  </a></li>
+			  <li><a class="button" href="https://untplp.com/PEE.html"> Professional Experience and Education </a></li>
+			  <li><a class="button" href="https://untplp.com/mentoring.html"> Mentoring Details </a></li>
+			</ul>
+		</div>
+	
+		<div class="campus">
+			<!-- <div class = "plp"> -->
+				<h1> Professional Leadership Program
+				</h1>
+		</div>
+		<div class="wordy">
+			<h1> Thank you for your interest in becoming a mentor with the UNT Professional Leadership Program for the Fall 2023 - Spring 2024 academic year. </h1>
+			
+			<h3>	Our mentor application is multi-purpose: it serves as a screening tool to identify the right mentors for our program, helps us to match our mentors 
+						and students, and eventually serves as the "profile" that your student mentee will be able to view. After matching is completed, your Xinspire account 
+						will become your main connection platform with our program and your source for announcements, resources, events, and more. </h3>
+						
+			<h3> To be Elligible for the program, potential mentors: 
+				<ul>
+					<li> Must have at least 5 years of professional experience</li>
+					<li> Must either be referred to the program byu someone in the PLP network or complete a phone screening with PLP staff</li>
+					<li> Must not have been convicted of a felony</li>
+					<li> Must not presently be listed or ever have been listed on the National Sex Offender Public Registry</li>
+				</ul>
+			If you have already read our information page about who we are and what we are looking for in our mentors, we invite you to complete you application. Matching will take place during the late summer of 2023. 
+			</h3>
+			
+			<h3> Please note that there are five application sections to complete. It should take you 15-30 minutes, depending on your background. </h3>
+		</div>			
+		<div class="copyright">
+		  <p> <span> © </span> 2023 University of North Texas. All Rights Reserved.</p>
+		</div>
+		
+		<div class="footer">
+			<ul class="bottomTabs">
+				<li><a class="button2" href="https://speakout.unt.edu/">Mental Health Resources  </a> </li>
+			  <li><a class="button2" href="https://idea.unt.edu/">Inclusion Diversity Equity and Access</a> </li>
+			  <li><a class="button2" href="https://idea.unt.edu/title-ix">Title IX and Sexual Misconduct Policy</a></li>
+			  <li><a class="button2" href="https://www.unt.edu/ada"> AA/EOE/ADA </a></li>
+			  <li><a class="button2" href="https://www.unt.edu/disclaimer"> Disclaimer </a></li>
+			  <li><a class="button2" href="https://sao.fraud.texas.gov/"> State Auditor's Office Fraud, Waste, or Abuse Hotline </a></li>
+			  <li><a class="button2" href="https://policy.unt.edu/policy/04-002"> Requests for Public Information </a></li>
+			  <li><a class="button2" href="https://cm.maxient.com/reportingform.php?UnivofNorthTexas&amp;layout_id=6"> Report Sexual Misconduct </a></li>
+			  <li><a class="button2" href="https://studentaffairs.unt.edu/hazing"> Hazing Prevention and Response </a></li>
+			  <li><a class="button2" href="https://secure.ethicspoint.com/domain/media/en/gui/56566/index.html"> Compliance Trust Line </a></li>
+			  <li><a class="button2" href="https://gov.texas.gov/"> Texas Homeland Security </a></li>
+			  <li><a class="button2" href="https://itss.untsystem.edu/divisions/mrs/it-standards/linking-notice.php"> Linking Notice </a></li>
+			  <li><a class="button2" href="https://www.tsl.texas.gov/trail/index.html"> Statewide Search </a></li>
+			  <li><a class="button2" href="https://veterans.portal.texas.gov/"> Texas Veterans Portal </a></li>
+			  <li><a class="button2" href="https://www.unt.edu/compact-with-texans"> Compact With Texans </a></li>
+			  <li><a class="button2" href="https://www.texas.gov/"> Texas Gov </a></li>
+			  <li><a class="button2" href="https://www.unt.edu/required-links.html"> Required Links </a></li>
+			</ul>
+		
+		</div>
+	
+</body></html>
