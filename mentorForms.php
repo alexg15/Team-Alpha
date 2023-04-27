1f8c937c0194a594b8774da1f72957dc52c96a9b
alexg15 <34933026+alexg15@users.noreply.github.com>
2023-04-27 13:56:27 -0500

    Add files via upload

---
+++ mentorForms.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html>
+	<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>UNT Professional Leadership program </title>
+		<meta name="description" content="Professional Leadership program">
+		<meta name="keywords" content="Professional Leadership Program UNT">
+		<link rel="stylesheet" type="text/css" href="./css/main.css">
+    <link href="./css/css" rel="stylesheet" type="text/css">
+		<style></style>
+	</head>
+
+<body>
+<div class = "header">
+    <ul class = "tabs">
+        <li><a class="button" href="mwelcome.html">Welcome </a></li>
+      <li><a class="button" href="mentorForms.html">Requirements  </a></li>
+      <li><a class="button" href="">Education and Experience  </a></li>
+      <li><a class="button" href=""> Mentoring Details </a></li>
+      <li><b> Basic Info </b></li>
+    </ul>
+</div>
+
+<h1> Basic Info </h1>
+<div class = "ferpa">
+    <form action="/action_pagebasic.php">
+    <label for="fname">First Name:</label><br>
+    <input type="text" id="fname" name="fname" value=""><br>
+    <label for="pname">Preferred First Name:</label><br>
+    <input type="text" id="pname" name="pname" value=""><br>
+    <label for="lname">Last Name:</label><br>
+    <input type="text" id="lname" name="lname" value=""><br><br>
+    <label for="ethnicity">Ethnicity:</label><br>
+    <input type="text" id="ethnicity" name="ethnicity" value=""><br><br>
+    <label for="untalumni">Are you an alumni of UNT?</label><br>
+    <input type="text" id="untalumni" name="untalumni" value=""><br><br>
+    <label for="plpalumni">Are you an alumni of PLP?: </label><br>
+    <input type="text" id="plpalumni" name="plpalumni" value=""><br><br>
+    <label for="served">Have you previously served as a PLP mentor?</label><br>
+    <input type="text" id="served" name="served" value=""><br><br>
+    <label for="yearsserved">For how many consecutive years have you served as PLP mentor?</label><br>
+    <input type="text" id="yearsserved" name="yearsserved" value=""><br><br>
+    <label for="phnumber">Preferred Phone Number </label><br>
+    <input type="text" id="phnumber" name="phnumber" value=""><br><br>
+    <label for="streetadd">Permanent Street Address:</label><br>
+    <input type="text" id="streetadd" name="streetadd" value=""><br><br>
+    <label for="city">City:</label><br>
+    <input type="text" id="city" name="city" value=""><br><br>
+    <label for="state">State</label><br>
+    <input type="text" id="state" name="state" value=""><br><br>
+    <label for="zip">Zip Code</label><br>
+    <input type="text" id="zip" name="zip" value=""><br><br>
+    <!--y/n question -->
+    <label for="diffadd">Does your mailing address differ from your permanent address?</label><br>
+    <input type="text" id="differadd" name="differadd" value=""><br><br>
+    <!-- if yes, then all these fields drop down -->
+    <label for="diffmail">Mailing Street Address </label><br>
+    <input type="text" id="diffmail" name="diffmail" value=""><br><br> 
+    <label for="diffcity">Mailing City </label><br>
+    <input type="text" id="diffcity" name="diffcity" value=""><br><br> 
+    <label for="diffstate">Mailing State</label><br>
+    <input type="text" id="diffstate" name="diffstate" value=""><br><br>
+    <label for="diffzip">Mailing Zip Code</label><br>
+    <input type="text" id="diffzip "name="differadd" value=""><br><br>
+    <label for="citizen">Country of Citizenship:</label><br>
+    <input type="text" id="citizen" name="citizen" value=""><br><br> 
+    <label for="lang">What Languages do you Speak?</label><br>
+    <input type="text" id="lang" name="lang" value=""><br><br>
+    </form> 
+</div>
+
+
+<h1> Mentoring Details</h1>
+<div class = "ferpa">
+  <form action="/action_pagebasic.php">
+  <label for="fname"> Why do you want to be a PLP mentor? </label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  <label for="fname"> What are your goals / expectations for the mentoring relationship? </label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  <label for="genderp"> Do you have a mentee gender preference?</label><br>
+  <input type="text" id="genderp" name="genderp" value=""><br><br>
+  <label for="genderpref"> Gender Preffered</label><br>
+  <input type="text" id="genderpref" name="genderpref" value=""><br><br>
+  <label for="prefstudmaj"> Taking into account your education and professional experience, which student majors would you prefer to mentor?</label><br>
+  <input type="text" id="prefstudmaj" name="prefstudmaj" value=""><br><br>
+  <label for="transport"> Some students do not have access to transportation. Based on your willingness and ability to travel, should we pair you with a mentee that has a car?</label><br>
+  <input type="text" id="transport" name="transport" value=""><br><br>
+  <label for="topics"> Some students do not have access to transportation. Based on your willingness and ability to travel, should we pair you with a mentee that has a car?</label><br>
+  <input type="text" id="topics" name="topics" value=""><br><br>
+  </form> 
+</div>
+
+
+
+<h1> Mentor Requirements </h1>
+<div class = "ferpa">
+  <form action="/action_pagebasic.php">
+  <label for="yearsExp">How many years of professional experience do you have?</label><br>
+  <input type="text" id="yearsExp" name="yearsExp" value=""><br>
+  <label for="reference">Who referred you to mentor with PLP</label><br>
+  <input type="text" id="reference" name="reference" value=""><br>
+  <label for="refEmail">Provide their email address: </label><br>
+  <input type="text" id="refEmail" name="refEmail" value=""><br><br>
+  <label for="refAffil">What is their affiliation with PLP?</label><br>
+  <input type="text" id="refAffil" name="refAffil" value=""><br><br>
+  
+  <h1> Program expectations </h1>
+  <label for="acknMatch">Matching is completed during the summer based on program needs.�I understand that I may not be paired with a student this year.� </label><br>
+  <input type="checkbox" id="" name="acknMatch" value=""><br><br>
+  <label for="acknMeet"> I will be able to meet with my mentee for the full academic year. </label><br>
+  <input type="checkbox" id="" name="acknMeet" value=""><br><br>
+  <label for="acknShare">By checking this box, I understand that as a mentor, my email address, employer name, and job title will be shared with the PLP community (students and mentors).� </label><br>
+  <input type="checkbox" id="" name="acknShare" value=""><br><br>
+  </form> 
+</div>
+
+
+
+<h1> Education and Experience </h1>
+  <div class = "ferpa">
+  <form action="/action_pagebasic.php">
+  <label for="summary">Provide brief professional summary.</label><br>
+  <input type="text" id="summary" name="summary" value=""><br>
+  <label for="indExp">In order to complete the matching process, we consider your industry experience. In which industry/industries do you have experience? </label><br>
+  <input type="text" id="indExp" name="indExp" value=""><br>
+  
+  <h1> Current Job Info</h1>
+  <label for="compName">Company Name</label><br>
+  <input type="text" id="compName" name="compName" value=""><br><br>
+  <label for="jobTitle">Job Title</label><br>
+  <input type="text" id="jobTitle" name="jobTitle" value=""><br><br>
+  <label for="jobDescr">Briefly describe this position</label><br>
+  <input type="text" id="jobDescr" name="jobDescr" value=""><br><br>
+  <label for="career">Career Field </label><br>
+  <input type="text" id="career" name="career" value=""><br><br>
+  <label for="startEmploy">Start of employment</label><br>
+  <input type="text" id="startEmploy" name="startEmploy" value=""><br><br>
+
+  <h1> Previous Work Experience </h1> 
+  <label for="fCompName">Former Company Name</label><br>
+  <input type="text" id="fCompName" name="fCompName" value=""><br><br>
+  <label for="fJobTitle">Former job title </label><br>
+  <input type="text" id="fJobTitle" name="fJobTitle" value=""><br><br>
+  <label for="fJobDescr"> briefly describe this position</label><br>
+  <input type="text" id="fJobDescr" name="fJobDescr" value=""><br><br>
+  <label for="stateEmploy"> State of employment</label><br>
+  <input type="text" id="stateEmploy" name="stateEmploy" value=""><br><br>
+  <label for="endEmploy"> End of employment</label><br>
+  <input type="text" id="endEmploy" name="endEmploy" value=""><br><br>
+
+  <h1> Education </h1>
+  <label for="uniName"> Institution Name</label><br>
+  <input type="text" id="uniName" name="uniName" value=""><br><br>
+  <label for="degree"> Degree</label><br>
+  <input type="text" id="degree" name="degree" value=""><br><br>
+  <label for="major"> Major</label><br>
+  <input type="text" id="major" name="major" value=""><br><br>
+  <label for="minor"> Minor</label><br>
+  <input type="text" id="minor" name="minor" value=""><br><br>
+  <label for="gradYear"> Graduation Year</label><br>
+  <input type="text" id="gradYear" name="gradYear" value=""><br><br>
+  <label for="certs"> Professional Certifactions </label><br>
+  <input type="text" id="certs" name="certs" value=""><br><br>
+  <label for="acadAffil"> Professional academic Affiliations</label><br>
+  <input type="text" id="acadAffil" name="acadAffil" value=""><br><br>
+
+  <input type="submit" value="Submit">
+  </form> 
+</div>
+</body>
+</html>
+
---
+++ mwelcome.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+	<head>
+    <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>UNT Professional Leadership program </title>
+		<meta name="description" content="Professional Leadership program">
+		<meta name="keywords" content="Professional Leadership Program UNT">
+		<link rel="stylesheet" type="text/css" href="./css/main.css">
+    <link href="./css/css" rel="stylesheet" type="text/css">
+	<style></style>
+	</head>
+	
+	<body>
+	<div class="campus">
+		<!-- <div class = "plp"> -->
+	<h1> Professional Leadership Program</h1>
+	</div>
+	<button onclick="window.location.href='index.html';">Log Out</button>
+	<div class = "wordy">
+		<h1> Thank you for your interest in becoming a mentor with the UNT Professional Leadership Program for the Fall 2023 - Spring 2024 academic year. </h1>
+		
+		<h3>	Our mentor application is multi-purpose: it serves as a screening tool to identify the right mentors for our program, helps us to match our mentors 
+					and students, and eventually serves as the "profile" that your student mentee will be able to view. After matching is completed, your Xinspire account 
+					will become your main connection platform with our program and your source for announcements, resources, events, and more. 
+		</h3>			
+		<h3> To be Elligible for the program, potential mentors: 
+			<ul>
+				<li> Must have at least 5 years of professional experience</li>
+				<li> Must either be referred to the program byu someone in the PLP network or complete a phone screening with PLP staff</li>
+				<li> Must not have been convicted of a felony</li>
+				<li> Must not presently be listed or ever have been listed on the National Sex Offender Public Registry</li>
+			</ul>
+		If you have already read our information page about who we are and what we are looking for in our mentors, we invite you to complete you application. Matching will take place during the late summer of 2023. 
+		</h3>
+		
+		<h3> Please note that there are five application sections to complete. It should take you 15-30 minutes, depending on your background. 
+		</h3>
+	</div>
+<input type="checkbox" id="" name="timeCheck" value=""> I understand this application must be completed in one sitting and wish to continue
+</p>
+
+<br>
+<input type="submit" value="Continue">
+	</div>			
+	<div class="copyright">
+	  <p> <span> © </span> 2023 University of North Texas. All Rights Reserved.</p>
+	</div>
+</body>
+</html>
